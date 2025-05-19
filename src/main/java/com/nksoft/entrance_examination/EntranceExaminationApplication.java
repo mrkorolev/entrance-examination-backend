28968cc58a750aa01c8ba8ac798f3ab746ba91d8
Nikita Korolev <nk25.dev@gmail.com>
2025-05-19 03:17:15 +0300

    Add spring security, instantiate BCryptPasswordEncoder

--- src/main/java/com/nksoft/entrance_examination/EntranceExaminationApplication.java
+++ src/main/java/com/nksoft/entrance_examination/EntranceExaminationApplication.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -16,5 +18,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EntranceExaminationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EntranceExaminationApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
