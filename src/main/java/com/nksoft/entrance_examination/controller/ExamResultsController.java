d9915ae841a8bd67cb25835dc5d014f67bfde43d
Nikita Korolev <nk25.dev@gmail.com>
2025-06-10 13:23:34 +0300

    ExamResult template

---
+++ src/main/java/com/nksoft/entrance_examination/controller/ExamResultsController.java
@@ -0,0 +1,12 @@
+package com.nksoft.entrance_examination.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/exam-results")
+public class ExamResultsController {
+    // TODO: finish controller, only GET and POST requests allowed!
+}
---
+++ src/main/java/com/nksoft/entrance_examination/repository/ExamResultRepository.java
@@ -0,0 +1,8 @@
+package com.nksoft.entrance_examination.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExamResultRepository {
+    // TODO: add repository methods
+}
