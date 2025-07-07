9397404dc3b055e1ec6f5c244d07d97f616e43db
Nikita Korolev <nk25.dev@gmail.com>
2025-07-08 00:17:30 +0300

    ExamResult functionality update, NativeConfig add Pageable

---
+++ src/main/java/com/nksoft/entrance_examination/examination/dto/ExamResultDto.java
@@ -0,0 +1,29 @@
+package com.nksoft.entrance_examination.examination.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.nksoft.entrance_examination.examination.model.BookletType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ExamResultDto {
+    private Long resultId;
+    private Long studentCode;
+    private Long examEntryId;
+    private Long examId;
+    private BookletType bookletType;
+    private String rawAnswers;
+    private int correctCount;
+    private int incorrectCount;
+    private int unansweredCount;
+    private Float netScore;
+    private Float rescaledScore;
+    private Float finalScore;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdDate;
+}
