83cfb3af260d6c3bc187b6e415a9af217cf37c6e
Nikita Korolev <nk25.dev@gmail.com>
2025-07-15 21:19:57 +0300

    Add ProfileExecution aspect to log resources and/or time spent for method execution

---
+++ src/main/java/com/nksoft/entrance_examination/common/aspect/ProfileExecutionAspect.java
@@ -0,0 +1,55 @@
+package com.nksoft.entrance_examination.common.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ProfileExecutionAspect {
+    @Around("@annotation(profileExecution)")
+    public Object profile(ProceedingJoinPoint joinPoint,
+                          ProfileExecution profileExecution) throws Throwable {
+        Runtime runtime = Runtime.getRuntime();
+        boolean logMemory = profileExecution.logMemory();
+
+        long usedBefore = 0;
+        if (logMemory) {
+            usedBefore = (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024);
+        }
+
+        long start = System.nanoTime();
+        Object result = joinPoint.proceed();
+        long end = System.nanoTime();
+
+        long usedAfter = 0;
+        long heapDiff = 0;
+        if (logMemory) {
+            usedAfter = (runtime.totalMemory() - runtime.freeMemory()) / (1024 * 1024);
+            heapDiff = usedAfter - usedBefore;
+        }
+
+        long durationMs = (end - start) / 1_000_000;
+        long minutes = durationMs / 1000 / 60;
+        long seconds = (durationMs / 1000) % 60;
+
+        if (logMemory) {
+            log.info(
+                    "{} executed in {} ms ({} min {} sec), heap: {} MB → {} MB (diff: {} MB)",
+                    joinPoint.getSignature(),
+                    durationMs, minutes, seconds,
+                    usedBefore, usedAfter, heapDiff
+            );
+        } else {
+            log.info(
+                    "{} executed in {} ms ({} min {} sec)",
+                    joinPoint.getSignature(),
+                    durationMs, minutes, seconds
+            );
+        }
+        return result;
+    }
+}
